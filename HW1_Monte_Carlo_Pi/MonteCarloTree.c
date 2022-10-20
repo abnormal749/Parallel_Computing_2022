ebd995ed0a53ca5c46c864e22595f7272f00cb74
abnormal749 <40458788+abnormal749@users.noreply.github.com>
2022-10-20 23:28:19 +0800

    update

---
+++ HW1_Monte_Carlo_Pi/MonteCarloTree.c
@@ -0,0 +1,70 @@
+/*  HW2 by Matthew Chen Q36091334 */
+
+#include <stdio.h>     // printf()
+#include <stdlib.h>
+#include <limits.h>    // UINT_MAX
+#include <time.h>
+#include <stdint.h>
+#include "mpi.h"
+
+#define distance_squared(x,y) x*x+y*y
+
+int main (int argc, char *argv[]) {
+   int    id,numprocs;
+   unsigned n;
+   double startTime=0.0, totalTime;
+   int    localCount=0, globalCount=0;
+   int    namelen, remaindPart;
+   char   processor_name[MPI_MAX_PROCESSOR_NAME];
+
+   MPI_Init(&argc,&argv);
+   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+   MPI_Comm_rank(MPI_COMM_WORLD,&id);
+   MPI_Get_processor_name(processor_name,&namelen);
+
+   if (id == 0)
+        startTime = MPI_Wtime();
+
+   n = 1000000000;        //total number of darts
+   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+   srand(time(NULL));
+   unsigned portion = n/numprocs;
+   for (unsigned i = 0; i < portion; i++){
+       double x,y;
+       x = ((double)rand() / (double)RAND_MAX);
+       y = ((double)rand() / (double)RAND_MAX);
+       if (distance_squared(x,y) <= 1)
+           localCount++;
+   }
+
+   remaindPart = numprocs;
+
+   for(; remaindPart!=1;){
+       int maxElement = remaindPart;
+       remaindPart = (remaindPart&1) + (remaindPart>>1);
+       if(id<remaindPart){
+           if(id+remaindPart < maxElement){
+               MPI_Status status;
+               int partnerCount = 0;
+               MPI_Recv(&partnerCount, 1, MPI_INTEGER, id+remaindPart,0,MPI_COMM_WORLD,&status);
+               localCount += partnerCount;
+           }
+       }else if(id<maxElement){
+           MPI_Send(&localCount, 1, MPI_INTEGER, id-remaindPart, 0, MPI_COMM_WORLD);
+       }
+   }
+
+   //MPI_Reduce(&localCount, &globalCount, 1, MPI_UNSIGNED , MPI_SUM, 0, MPI_COMM_WORLD);
+
+   if (id == 0){
+      totalTime = MPI_Wtime() - startTime;
+      globalCount = localCount;
+      printf("CircuitSAT finished in time %f secs.\n", totalTime);
+      printf("The solutions were: %.10f\n\n", 4*(globalCount/((double) n)));
+      fflush(stdout);
+   }
+
+   MPI_Finalize();
+   return 0;
+}
